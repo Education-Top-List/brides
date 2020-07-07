5dbe2f42b151dfa364e7d25313faf8736391bff7
Your Name <you@example.com>
2020-07-08 00:20:12 +0700

    push

--- wp-content/themes/doanhnghiep/page-template-trangchu.php
+++ wp-content/themes/doanhnghiep/page-template-trangchu.php
@@ -8,15 +8,7 @@ get_header();
 	<div id="content">
 		<div class="g_content">
 			<div class="content_left">
-				<div class="content_post_admin">
-					<?php 
-					$content_post = get_post($my_postid);
-					$content = $content_post->post_content;
-					$content = apply_filters('the_content', $content);
-					$content = str_replace(']]>', ']]&gt;', $content);
-					echo $content;
-					?>
-				</div>
+		
 				
 			</div><!-- content_left -->
 		</div>
